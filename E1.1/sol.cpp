cb2c65e51ab2bf89bfe6ae3776cc5ee1e5baeba0
derek_lee <20820343@student.curtin.edu.au>
2023-01-08 00:21:14 +0400

    Tried recursion

--- E1.1/sol.cpp
+++ E1.1/sol.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-  int x;
-  std::cout << "Enter an integer: ";
-  std::cin >> x;
-
-  std::cout << "Double that number is: " << x * 2 << std::endl;
-
-  return 0;
+int factorialfun(int num)
+{
+	if (num>0)
+	{
+		return num*factorialfun(num-1);
+	}
+	else
+	{
+		return 1;
+	}
+}
+int main()
+{
+	int num;
+	cin>>num;
+	cout<<factorialfun(num);
 }
