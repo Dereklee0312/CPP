aa106bb2c024adf74d3d04b37ed186f958776526
derek_lee <20820343@student.curtin.edu.au>
2023-01-09 23:22:03 +0400

    Tested with preprocessor concepts

--- E1.2/main.cpp
+++ E1.2/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int add(int x, int y); // needed so main.cpp knows that add() is a function defined elsewhere
 
 int main()
 {
-    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+    cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
     return 0;
 }
